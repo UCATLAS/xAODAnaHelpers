14e3cb62e38541f4d357a50751c9c6599515e0ca
Giordon Stark <kratsg@gmail.com>
2015-02-19 12:57:29 +0000

    This makes test_multiAlgo run again. There are 3 unchecked successes. I've commented out the Electron, Muon calibrators... as well as the OverlapRemoval tool.

--- util/test_multiAlgo.cxx
+++ util/test_multiAlgo.cxx
@@ -53,8 +53,8 @@ int main( int argc, char* argv[] ) {
   BasicEventSelection* baseEventSel             = new BasicEventSelection(  "baseEventSel",             localDataDir+"baseEvent.config");
 
   JetCalibrator* jetCalib                       = new JetCalibrator(        "jetCalib_AntiKt4TopoEM",   localDataDir+"jetCalib_AntiKt4TopoEMCalib.config");
-  MuonCalibrator* muonCalib                     = new MuonCalibrator(       "muonCalib",                localDataDir+"muonCalib.config");
-  ElectronCalibrator* electronCalib             = new ElectronCalibrator(   "electronCalib",            localDataDir+"electronCalib.config");
+  // MuonCalibrator* muonCalib                     = new MuonCalibrator(       "muonCalib",                localDataDir+"muonCalib.config");
+  // aElectronCalibrator* electronCalib             = new ElectronCalibrator(   "electronCalib",            localDataDir+"electronCalib.config");
 
   JetSelector* jetSelect_signal                 = new JetSelector(          "jetSelect_signal",         localDataDir+"jetSelect_signal.config");
   JetHistsAlgo* jetHistsAlgo_signal             = new JetHistsAlgo(         "jetHistsAlgo_signal",      localDataDir+"jetHistsAlgo_signal.config");
@@ -62,20 +62,20 @@ int main( int argc, char* argv[] ) {
   JetSelector* jetSelect_truth                  = new JetSelector(          "jetSelect_truth",          localDataDir+"jetSelect_truth.config");
   JetHistsAlgo* jetHistsAlgo_truth              = new JetHistsAlgo(         "jetHistsAlgo_truth",       localDataDir+"jetHistsAlgo_truth.config");
 
-  OverlapRemover* overlapRemoval                = new OverlapRemover(       "OverlapRemovalTool",       localDataDir+"overlapRemoval.config");
+  // OverlapRemover* overlapRemoval                = new OverlapRemover(       "OverlapRemovalTool",       localDataDir+"overlapRemoval.config");
   JetHistsAlgo* jk_AntiKt10LC                   = new JetHistsAlgo(         "AntiKt10/",                localDataDir+"test_jetPlotExample.config");
 
 
   // Attach algorithms
   job.algsAdd( baseEventSel );
   job.algsAdd( jetCalib );
-  job.algsAdd( muonCalib );
-  job.algsAdd( electronCalib );
+  // job.algsAdd( muonCalib );
+  // job.algsAdd( electronCalib );
   job.algsAdd( jetSelect_signal );
   job.algsAdd( jetHistsAlgo_signal );
   job.algsAdd( jetSelect_truth );
   job.algsAdd( jetHistsAlgo_truth );
-  job.algsAdd( overlapRemoval );
+  // job.algsAdd( overlapRemoval );
   job.algsAdd( jk_AntiKt10LC );
 
   // Run the job using the local/direct driver:
