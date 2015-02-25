abe67a1f8e707de38b4465e5be4e8c390fde4742
Giordon Stark <kratsg@gmail.com>
2015-02-25 10:26:01 +0000

    HelperFunctions::jetReclustering and HelperFunctions::jetTrimming added.

--- xAODAnaHelpers/HelperFunctions.h
+++ xAODAnaHelpers/HelperFunctions.h
@@ -4,6 +4,9 @@
 // local includes
 #include "AsgTools/StatusCode.h"
 
+// jet reclustering and trimming
+#include <fastjet/JetDefinition.hh>
+
 #ifndef __CINT__
   #include "xAODTracking/VertexContainer.h"
   #include "xAODEgamma/ElectronContainer.h"
@@ -35,6 +38,57 @@ namespace HelperFunctions {
   const xAOD::Vertex* getPrimaryVertex(const xAOD::VertexContainer* vertexContainer);
   int getPrimaryVertexLocation(const xAOD::VertexContainer* vertexContainer);
   std::string replaceString(std::string subjet, const std::string& search, const std::string& replace);
+
+  /*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*\
+  |                                                                            |
+  |   Author  : Giordon Stark                                                  |
+  |   Email   : gstark@cern.ch                                                 |
+  |   Thanks to Ben Nachman for inspiration                                    |
+  |                                                                            |
+  |   jetReclustering():                                                       |
+  |       Takes a set of small-R jets and reclusters to large-R jets           |
+  |                                                                            |
+  |       @jets                                                                |
+  |         jet container to recluster and trim                                |
+  |       @radius   [1.0]                                                      |
+  |         radius of large-R jet                                              |
+  |       @fcut     [0.05]                                                     |
+  |         trimming cut to apply                                              |
+  |       @rc_alg   [fastjet::kt_algorithm]                                    |
+  |         clustering algorithm                                               |
+  |                                                                            |
+  |                                                                            |
+  |   jetTrimming():                                                           |
+  |       Takes a jet or a set of jets and applies an fcut on subjets          |
+  |         and reclusters into a TLorentzVector                               |
+  |                                                                            |
+  |       @jet(s)                                                              |
+  |         jet (container to loop over and) to apply trimming                 |
+  |       @radius   [0.3]                                                      |
+  |         radius of subjets to form                                          |
+  |       @fcut     [0.05]                                                     |
+  |         trimming cut to apply on subjets                                   |
+  |       @s_alg    [fastjet::kt_algorithm]                                    |
+  |         algorithm for building subjets                                     |
+  |                                                                            |
+  \*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*/
+  std::vector<TLorentzVector> jetReclustering(
+    const xAOD::JetContainer* jets,
+    double radius = 1.0,
+    double fcut = 0.05,
+    fastjet::JetAlgorithm rc_alg = fastjet::antikt_algorithm);
+  /* http://www.lpthe.jussieu.fr/~salam/fastjet/repo/doxygen-3.0alpha2/classfastjet_1_1Filter.html#usage */
+  std::vector<TLorentzVector> jetTrimming(
+    const xAOD::JetContainer* jets,
+    double radius = 0.3,
+    double fcut = 0.05,
+    fastjet::JetAlgorithm s_alg = fastjet::kt_algorithm);
+  TLorentzVector jetTrimming(
+    const xAOD::Jet* jet,
+    double radius = 0.3,
+    double fcut = 0.05,
+    fastjet::JetAlgorithm s_alg = fastjet::kt_algorithm);
+
 
   // miscellaneous
   bool sort_pt(xAOD::IParticle* partA, xAOD::IParticle* partB);
