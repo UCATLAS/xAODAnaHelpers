f9cd6b76ad6254a3a4c7aba8b8253a0d97d94728
Giordon Stark <kratsg@gmail.com>
2015-06-25 20:44:16 -0500

    For discussing #134.

--- Root/Algorithm.cxx
+++ Root/Algorithm.cxx
@@ -6,6 +6,10 @@
 // RCU include for throwing an exception+message
 #include <RootCoreUtils/ThrowMsg.h>
 
+//  for isMC()
+#include <xAODAnaHelpers/HelperFunctions.h>
+#include "xAODEventInfo/EventInfo.h"
+
 // this is needed to distribute the algorithm to the workers
 ClassImp(xAH::Algorithm)
 
@@ -15,6 +19,7 @@ xAH::Algorithm::Algorithm() :
   m_systName(""),
   m_systVal(0),
   m_eventInfoContainerName("EventInfo"),
+  m_isMC(-1),
   m_configName(""),
   m_event(nullptr),
   m_store(nullptr)
@@ -61,4 +66,25 @@ xAH::Algorithm* xAH::Algorithm::setSyst(std::string systName, float systVal){
   m_systName = systName;
   m_systVal = systVal;
   return this;
+}
+
+int xAH::Algorithm::isMC(){
+  // first override if need to
+  if(m_isMC == 0 || m_isMC == 1) return m_isMC;
+
+  const xAOD::EventInfo* ei(nullptr);
+  // couldn't retrieve it
+  if(!HelperFunctions::retrieve(ei, m_eventInfoContainerName, m_event, m_store).isSuccess()){
+    if(m_debug) Warning("isMC()", "Could not retrieve eventInfo container: %s", m_eventInfoContainerName.c_str());
+    return -1;
+  }
+
+  static SG::AuxElement::ConstAccessor<uint32_t> eventType("eventTypeBitmask");
+  if(!eventType.isAvailable(*ei)){
+    if(m_debug) Warning("isMC()", "eventType is not available.");
+    return -1;
+  }
+
+  // reached here, return 0 or 1 since we have all we need
+  return (static_cast<uint32_t>(eventType(*ei)) & xAOD::EventInfo::IS_SIMULATION);
 }
