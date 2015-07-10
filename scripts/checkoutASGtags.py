c1fb9bb14dd078e038c02b9f28efbb65dda6b9ac
Jeff Dandoy <jdandoy@gmail.com>
2015-07-10 10:07:36 +0200

    Adding new JER package

--- scripts/checkoutASGtags.py
+++ scripts/checkoutASGtags.py
@@ -29,7 +29,8 @@ dict_pkg = {'2.3.15': ["atlasoff/Reconstruction/Jet/JetUncertainties/tags/JetUnc
                        "atlasoff/PhysicsAnalysis/AnalysisCommon/PileupReweighting/tags/PileupReweighting-00-03-03",
                        "atlasoff/PhysicsAnalysis/AnalysisCommon/IsolationSelection/tags/IsolationSelection-00-00-05",
                        "atlasoff/Event/xAOD/xAODTracking/tags/xAODTracking-00-13-15"],
-            '2.3.18': ["atlasoff/Reconstruction/Jet/JetUncertainties/tags/JetUncertainties-00-09-28"]
+            '2.3.18': ["atlasoff/Reconstruction/Jet/JetUncertainties/tags/JetUncertainties-00-09-28",
+                       "atlasoff/Reconstruction/Jet/JetResolution/tags/JetResolution-03-00-36"]
            }
 
 try:
