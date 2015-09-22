f5ee31e8499565d37730ee68890f11801e2ede40
John Alison <johnda102@gmail.com>
2015-09-22 07:26:33 -0500

    Fix for carrige return

--- scripts/xAH_run.py
+++ scripts/xAH_run.py
@@ -285,7 +285,6 @@ if __name__ == "__main__":
             for line in f:
               if line.startswith('#') : continue
               if not line.strip()     : continue
-              if len(line) < 2        
               if use_scanDQ2:
                 ROOT.SH.scanDQ2(sh_all, line.rstrip())
               elif use_scanEOS:
