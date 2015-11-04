64364c31f13f9eb05d550214d1d6a906a06ddb1b
Giordon Stark <kratsg@gmail.com>
2015-11-04 17:52:16 -0500

    Update documentation for HistogramManager

--- xAODAnaHelpers/HistogramManager.h
+++ xAODAnaHelpers/HistogramManager.h
@@ -1,6 +1,14 @@
 #ifndef xAODAnaHelpers_HistogramManager_H
 #define xAODAnaHelpers_HistogramManager_H
 
+/** @file HistogramManager.h
+ *  @brief Manage your histograms
+ *  This is used by any class extending to pre-define a set of histograms to book by default.
+
+ *  @author Giordon Stark
+ *  @bug No known bugs
+ */
+
 #include <ctype.h>
 #include <TH1.h>
 #include <TH1F.h>
@@ -15,93 +23,132 @@
 class HistogramManager {
 
   protected:
-    // generically the main name assigned to all histograms
+    /** @brief generically the main name assigned to all histograms */
     std::string m_name;
-    // a detail level in the form of a string
+    /** @brief a detail level in the form of a string */
     std::string m_detailStr;
-    // a container holding all generated histograms
-    //  - loop over this to record to EL output
+    /** @brief a container holding all generated histograms
+     *  Loop over this to record to EL output */
     std::vector< TH1* > m_allHists; //!
 
   public:
-    // initializer and destructor
     HistogramManager(std::string name, std::string detailStr);
     virtual ~HistogramManager();
 
-    // this is used by any class extending to pre-define a set of histograms
-    //      to book by default
-    //  the following should be defined in other classes
+    // the following should be defined in other classes
     virtual StatusCode initialize(){      return StatusCode::SUCCESS; };
     virtual StatusCode execute(){         return StatusCode::SUCCESS; };
     virtual StatusCode finalize(){        return StatusCode::SUCCESS; };
 
-    // @book - record a histogram and call various functions
-    //      ** This is an overloaded function. It will build the right histogram
-    //          given the correct number of input arguments.**
-    //  @book.name - name of histogram, access it in ROOT file like "h_jetPt->Draw()"
-    //  @book.title - usually pointless,put a description of the histogram in here
-    //  @book.xlabel - label to put on the x-axis
-    //  @book.xbins - number of xbins to use
-    //  @book.xlow - lower bound on xbins
-    //  @book.xhigh - upper bound on xbins
-    //  @book.ylabel - label to put on the y-axis
-    //  @book.ylow - lower bound on ybins
-    //  @book.yhigh - upper bound on ybins
-    //  @book.zlabel - label to put on the z-axix
-    //  @book.zlow - lower bound on zbins
-    //  @book.zhigh - upper bound on zbins
-    //  @book.sumw2 - enable sumw2() for this histogram, default=True
+    /**
+     * @brief record a histogram and call various functions
+     * @note This is an overloaded function. It will build the right histogram
+     * given the correct number of input arguments.
+     *
+     *  @param name     name of histogram, access it in ROOT file like "h_jetPt->Draw()"
+     *  @param title    usually pointless,put a description of the histogram in here
+     *  @param xlabel   label to put on the x-axis
+     *  @param xbins    number of xbins to use
+     *  @param xlow     lower bound on xbins
+     *  @param xhigh    upper bound on xbins
+     *  @param xbinsArr variable xbins
+     *  @param ylabel   label to put on the y-axis
+     *  @param ylow     lower bound on ybins
+     *  @param yhigh    upper bound on ybins
+     *  @param ybinsArr variable ybins
+     *  @param zlabel   label to put on the z-axix
+     *  @param zlow     lower bound on zbins
+     *  @param zhigh    upper bound on zbins
+     *  @param zbinsArr variable zbins
+     */
     TH1F* book(std::string name, std::string title,
                std::string xlabel, int xbins, double xlow, double xhigh);
 
+    /**
+     * @overload
+     */
     TH2F* book(std::string name, std::string title,
                std::string xlabel, int xbins, double xlow, double xhigh,
                std::string xyabel, int ybins, double ylow, double yhigh);
 
+    /**
+     * @overload
+     */
     TH3F* book(std::string name, std::string title,
                std::string xlabel, int xbins, double xlow, double xhigh,
                std::string ylabel, int ybins, double ylow, double yhigh,
                std::string zlabel, int zbins, double zlow, double zhigh);
 
-
-    //// Variable Binned Histograms ////
+    /**
+     * @overload
+     */
     TH1F* book(std::string name, std::string title,
                std::string xlabel, int xbins, const Double_t* xbinsArr);
 
+    /**
+     * @overload
+     */
     TH2F* book(std::string name, std::string title,
                std::string xlabel, int xbins, const Double_t* xbinsArr,
                std::string ylabel, int ybins, double ylow, double yhigh);
 
+    /**
+     * @overload
+     */
     TH2F* book(std::string name, std::string title,
                std::string xyabel, int xbins, double xlow, double xhigh,
                std::string ylabel, int ybins, const Double_t* ybinsArr);
 
+    /**
+     * @overload
+     */
     TH2F* book(std::string name, std::string title,
                std::string xyabel, int xbins, const Double_t* xbinsArr,
                std::string ylabel, int ybins, const Double_t* ybinsArr);
 
+    /**
+     * @overload
+     */
     TH3F* book(std::string name, std::string title,
                std::string xlabel, int xbins, const Double_t* xbinsArr,
                std::string ylabel, int ybins, const Double_t* ybinsArr,
                std::string zlabel, int zbins, const Double_t* zbinsArr);
 
-
-
-    // Record all histograms from m_allHists to the worker
+    /**
+     * @brief record all histograms from HistogramManager#m_allHists to the worker
+     */
     void record(EL::Worker* wk);
 
   private:
-    // Turn on Sumw2 for the histogram
+    /**
+     * @brief Turn on Sumw2 for the histogram
+     *
+     * @param hist      The histogram to modify
+     * @param flag      Pass in whether to turn on Sumw2 or not
+     */
     void Sumw2(TH1* hist, bool flag=true);
 
-    // Push the new histogram to m_allHists
+    /**
+     * @brief Push the new histogram to HistogramManager#m_allHists
+     */
     void record(TH1* hist);
 
-    // Set the xlabel
+    /**
+     * @brief Set the labels on a histogram
+     *
+     * @param hist      The histogram to set the labels on
+     * @param xlabel    The xlabel to set
+     * @param ylabel    The ylabel to set
+     * @param zlabel    The zlabel to set
+     */
     void SetLabel(TH1* hist, std::string xlabel);
-    // Set the xlabel, ylabel
+    /**
+     * @overload
+     */
     void SetLabel(TH1* hist, std::string xlabel, std::string ylabel);
-    // Set the xlabel, ylabel, and zlabel
+    /**
+     * @overload
+     */
     void SetLabel(TH1* hist, std::string xlabel, std::string ylabel, std::string zlabel);
 
 };
