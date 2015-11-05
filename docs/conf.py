0dad61d5539293a92ffb0036df6c2c58fb756f5f
Giordon Stark <kratsg@gmail.com>
2015-11-04 23:30:39 -0500

    use c++ syntax highlighting

--- docs/conf.py
+++ docs/conf.py
@@ -111,6 +111,7 @@ exclude_patterns = ['_build']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
+highlight_language = 'c++'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
