8200c732d93aea9b860236e492e7df8cf5817ffc
Caterina <urania277@MacBook-Pro-7.local>
2015-12-23 12:37:59 +0100

    Getting HLT jets Prepared new class taking jets out of the TDT and putting them in the TStore

--- Root/HLTJetGetter.cxx
+++ Root/HLTJetGetter.cxx
@@ -19,9 +19,6 @@
 #include <EventLoop/Worker.h>
 
 // EDM include(s):
-<<<<<<< 23ea71dd850f66585ee1f7d2749bed218228fe87
-=======
-//CD: do we need all this stuff?
 #include "xAODEventInfo/EventInfo.h"
 #include "xAODJet/JetContainer.h"
 #include "xAODJet/JetAuxContainer.h"
@@ -46,49 +43,48 @@ using std::vector;
 ClassImp(HLTJetGetter)
 
 HLTJetGetter :: HLTJetGetter (std::string className) :
-    Algorithm(className),
-    m_triggerList(".*"),
-    m_inContainerName(""),
-    m_outContainerName(""),
-    m_trigDecTool(nullptr)
+Algorithm(className),
+m_triggerList(".*"),
+m_inContainerName(""),
+m_outContainerName(""),
+m_trigDecTool(nullptr)
 {
-  Info("HLTJetGetter()", "Calling constructor");
-
-  // read debug flag from .config file
-  m_debug                   = false;
-  //m_sort                    = true;
-
+    Info("HLTJetGetter()", "Calling constructor");
+    
+    // read debug flag from .config file
+    m_debug                   = false;
+    
 }
 
 
 EL::StatusCode HLTJetGetter :: setupJob (EL::Job& job)
 {
-  Info("setupJob()", "Calling setupJob");
-  job.useXAOD ();
-  xAOD::Init( "HLTJetGetter" ).ignore(); // call before opening first file
-  return EL::StatusCode::SUCCESS;
+    Info("setupJob()", "Calling setupJob");
+    job.useXAOD ();
+    xAOD::Init( "HLTJetGetter" ).ignore(); // call before opening first file
+    return EL::StatusCode::SUCCESS;
 }
 
 
 
 EL::StatusCode HLTJetGetter :: histInitialize ()
 {
-  RETURN_CHECK("xAH::Algorithm::algInitialize()", xAH::Algorithm::algInitialize(), "");
-  return EL::StatusCode::SUCCESS;
+    RETURN_CHECK("xAH::Algorithm::algInitialize()", xAH::Algorithm::algInitialize(), "");
+    return EL::StatusCode::SUCCESS;
 }
 
 
 
 EL::StatusCode HLTJetGetter :: fileExecute ()
 {
-  return EL::StatusCode::SUCCESS;
+    return EL::StatusCode::SUCCESS;
 }
 
 
 
 EL::StatusCode HLTJetGetter :: changeInput (bool /*firstFile*/)
 {
-  return EL::StatusCode::SUCCESS;
+    return EL::StatusCode::SUCCESS;
 }
 
 EL::StatusCode  HLTJetGetter :: configure ()
@@ -101,13 +97,13 @@ EL::StatusCode  HLTJetGetter :: configure ()
         
         // read debug flag from .config file
         m_debug                   = config->GetValue("Debug" , m_debug);
-        // input container to be read from TDT (will be stripped - TODO: put here stripped name already?)
+        // input container to be read from TDT
         m_inContainerName         = config->GetValue("InputContainer",  m_inContainerName.c_str());
         // output container is passed on with this output container name
         m_outContainerName        = config->GetValue("OutputContainer", m_outContainerName.c_str());
         // list of triggers whose features have to be pulled from TDT: ".*" for all
         m_triggerList             = config->GetValue("TriggerList", m_triggerList.c_str());
-
+        
         config->Print();
         
         delete config; config = nullptr;
@@ -120,7 +116,7 @@ EL::StatusCode  HLTJetGetter :: configure ()
     }
     
     if ( !getConfig().empty() )
-        Info("configure()", "JetCalibrator Interface succesfully configured! ");
+    Info("configure()", "JetCalibrator Interface succesfully configured! ");
     
     return EL::StatusCode::SUCCESS;
 }
@@ -128,88 +124,101 @@ EL::StatusCode  HLTJetGetter :: configure ()
 
 EL::StatusCode HLTJetGetter :: initialize ()
 {
-
-  Info("initialize()", "Initializing HLTJetGetter Interface... ");
-
-  m_event = wk()->xaodEvent();
-  m_store = wk()->xaodStore();
-
-  //
-  // Grab the TrigDecTool from the ToolStore
-  //
-
-  if ( asg::ToolStore::contains<Trig::TrigDecisionTool>( "TrigDecisionTool" ) ) {
-    m_trigDecTool = asg::ToolStore::get<Trig::TrigDecisionTool>("TrigDecisionTool");
-  } else {
-    Error("Initialize()", "the Trigger Decision Tool is not initialized.. [%s]", m_name.c_str());
-    return EL::StatusCode::FAILURE;
-  }
     
-  // Configure
-  if ( this->configure() == EL::StatusCode::FAILURE ) {
-    Error("initialize()", "Failed to properly configure. Exiting." );
-    return EL::StatusCode::FAILURE;
-  }
-
-  return EL::StatusCode::SUCCESS;
+    Info("initialize()", "Initializing HLTJetGetter Interface... ");
+    
+    m_event = wk()->xaodEvent();
+    m_store = wk()->xaodStore();
+    
+    //
+    // Grab the TrigDecTool from the ToolStore
+    //
+    
+    if ( asg::ToolStore::contains<Trig::TrigDecisionTool>( "TrigDecisionTool" ) ) {
+        m_trigDecTool = asg::ToolStore::get<Trig::TrigDecisionTool>("TrigDecisionTool");
+    } else {
+        Info ("Initialize()", "the Trigger Decision Tool is not yet initialized...[%s]. Doing so now.", m_name.c_str());
+        
+        m_trigConfTool = new TrigConf::xAODConfigTool( "xAODConfigTool" );
+        RETURN_CHECK("BasicEventSelection::initialize()", m_trigConfTool->initialize(), "Failed to properly initialize TrigConf::xAODConfigTool");
+        ToolHandle< TrigConf::ITrigConfigTool > configHandle( m_trigConfTool );
+        
+        m_trigDecTool = new Trig::TrigDecisionTool( "TrigDecisionTool" );
+        RETURN_CHECK("BasicEventSelection::initialize()", m_trigDecTool->setProperty( "ConfigTool", configHandle ), "");
+        RETURN_CHECK("BasicEventSelection::initialize()", m_trigDecTool->setProperty( "TrigDecisionKey", "xTrigDecision" ), "");
+        RETURN_CHECK("BasicEventSelection::initialize()", m_trigDecTool->setProperty( "OutputLevel", MSG::ERROR), "");
+        RETURN_CHECK("BasicEventSelection::initialize()", m_trigDecTool->initialize(), "Failed to properly initialize Trig::TrigDecisionTool");
+        Info("initialize()", "Successfully configured Trig::TrigDecisionTool!");
+    }
+    
+    // Configure
+    if ( this->configure() == EL::StatusCode::FAILURE ) {
+        Error("initialize()", "Failed to properly configure. Exiting." );
+        return EL::StatusCode::FAILURE;
+    }
+    
+    return EL::StatusCode::SUCCESS;
 }
 
 
 EL::StatusCode HLTJetGetter :: execute ()
 {
-  if ( m_debug ) { Info("execute()", "Getting HLT jets... "); }
-
-  //
-  // Create the new container and its auxiliary store.
-  //
-  xAOD::JetContainer*     hltJets    = new xAOD::JetContainer();
-  xAOD::JetAuxContainer*  hltJetsAux = new xAOD::JetAuxContainer();
-  hltJets->setStore( hltJetsAux ); //< Connect the two
-
-  //Retrieving jets via trigger decision tool:
-  const Trig::ChainGroup * chainGroup = m_trigDecTool->getChainGroup(m_triggerList.c_str()); //Trigger list:
-  auto chainFeatures = chainGroup->features(); //Gets features associated to chain defined above
-  std::string stripped_name = m_inContainerName;
-  stripped_name.erase(0, 23); //Stripping 'HLT_xAOD__JetContainer_' from input container name
-  auto JetFeatureContainers = chainFeatures.containerFeature<xAOD::JetContainer>(stripped_name.c_str());
-
-  for( auto fContainer : JetFeatureContainers ) {
-    for( auto trigJet : *fContainer.cptr() ) {
-      xAOD::Jet *Jet = new xAOD::Jet();
-      *Jet = *trigJet;
-      hltJets->push_back( Jet );
-    }//end trigJet loop
-  }//end feature container loop
-  
-  RETURN_CHECK("HLTJetGetter::execute()", m_store->record( hltJets,    m_outContainerName),     "Failed to record selected hltJets");
-  RETURN_CHECK("HLTJetGetter::execute()", m_store->record( hltJetsAux, m_outContainerName+"Aux."), "Failed to record selected hltJetsAux.");
-
-  if ( m_debug ) { m_store->print(); }
-
-  return EL::StatusCode::SUCCESS;
+    if ( m_debug ) { Info("execute()", "Getting HLT jets... "); }
+    
+    //
+    // Create the new container and its auxiliary store.
+    //
+    xAOD::JetContainer*     hltJets    = new xAOD::JetContainer();
+    xAOD::JetAuxContainer*  hltJetsAux = new xAOD::JetAuxContainer();
+    hltJets->setStore( hltJetsAux ); //< Connect the two
+    
+    //Retrieving jets via trigger decision tool:
+    const Trig::ChainGroup * chainGroup = m_trigDecTool->getChainGroup(m_triggerList.c_str()); //Trigger list:
+    auto chainFeatures = chainGroup->features(); //Gets features associated to chain defined above
+    auto JetFeatureContainers = chainFeatures.containerFeature<xAOD::JetContainer>(m_inContainerName.c_str());
+    
+    RETURN_CHECK("HLTJetGetter::execute()", m_store->record( hltJets,    m_outContainerName),     "Failed to record selected hltJets");
+    RETURN_CHECK("HLTJetGetter::execute()", m_store->record( hltJetsAux, m_outContainerName+"Aux."), "Failed to record selected hltJetsAux.");
+    
+    for( auto fContainer : JetFeatureContainers ) {
+        for( auto trigJet : *fContainer.cptr() ) {
+            xAOD::Jet *Jet = new xAOD::Jet();
+            *Jet = *trigJet;
+            hltJets->push_back( Jet );
+        }//end trigJet loop
+    }//end feature container loop
+    
+    if ( m_verbose ) { m_store->print(); }
+    
+    return EL::StatusCode::SUCCESS;
 }
 
 
 
 EL::StatusCode HLTJetGetter :: postExecute ()
 {
-  if ( m_debug ) { Info("postExecute()", "Calling postExecute"); }
-  return EL::StatusCode::SUCCESS;
+    if ( m_debug ) { Info("postExecute()", "Calling postExecute"); }
+    return EL::StatusCode::SUCCESS;
 }
 
 
 
 EL::StatusCode HLTJetGetter :: finalize ()
 {
-  Info("finalize()", "Deleting tool instances...");
-  return EL::StatusCode::SUCCESS;
+    Info("finalize()", "Deleting tool instances...");
+    
+    //question: are we doing this twice?
+    if ( m_trigDecTool )  {  m_trigDecTool = nullptr;  delete m_trigDecTool; }
+    if ( m_trigConfTool ) {  m_trigConfTool = nullptr; delete m_trigConfTool; }
+    
+    return EL::StatusCode::SUCCESS;
 }
 
 
 
 EL::StatusCode HLTJetGetter :: histFinalize ()
 {
-  Info("histFinalize()", "Calling histFinalize");
-  RETURN_CHECK("xAH::Algorithm::algFinalize()", xAH::Algorithm::algFinalize(), "");
-  return EL::StatusCode::SUCCESS;
+    Info("histFinalize()", "Calling histFinalize");
+    RETURN_CHECK("xAH::Algorithm::algFinalize()", xAH::Algorithm::algFinalize(), "");
+    return EL::StatusCode::SUCCESS;
 }
