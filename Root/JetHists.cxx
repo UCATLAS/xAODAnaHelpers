0283b4c8c6bd783871b7bd760a68552e80f7153d
Giordon Stark <kratsg@gmail.com>
2016-02-23 10:08:46 -0600

    forgot to add delete statement

--- Root/JetHists.cxx
+++ Root/JetHists.cxx
@@ -1151,6 +1151,7 @@ StatusCode JetHists::execute( const xAOD::IParticle* particle, float eventWeight
 
 StatusCode JetHists::finalize() {
     m_tracksInJet->finalize();
+    if(m_tracksInJet) delete m_tracksInJet;
     IParticleHists::finalize();
     return StatusCode::SUCCESS;
 }
