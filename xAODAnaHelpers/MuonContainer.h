5031baff438fca30bb7de8c625f6a77cef4412b1
John Alison <johnda102@gmail.com>
2016-07-25 09:16:33 -0500

    Connect mc option

--- xAODAnaHelpers/MuonContainer.h
+++ xAODAnaHelpers/MuonContainer.h
@@ -19,7 +19,7 @@ namespace xAH {
   class MuonContainer : public ParticleContainer<Muon,HelperClasses::MuonInfoSwitch>
     {
     public:
-      MuonContainer(const std::string& name = "muon", const std::string& detailStr="", float units = 1e3);
+      MuonContainer(const std::string& name = "muon", const std::string& detailStr="", float units = 1e3, bool mc = false);
       virtual ~MuonContainer();
     
       virtual void setTree(TTree *tree);
