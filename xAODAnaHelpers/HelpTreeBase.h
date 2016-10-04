2b1fbd5783702d106e02b0ac0f19eb0641b9acc3
Giordon Stark <kratsg@users.noreply.github.com>
2016-10-04 07:24:14 -0500

    Revert "Add met container"

--- xAODAnaHelpers/HelpTreeBase.h
+++ xAODAnaHelpers/HelpTreeBase.h
@@ -28,7 +28,6 @@
 
 #include "xAODAnaHelpers/HelperClasses.h"
 #include "xAODAnaHelpers/EventInfo.h"
-#include "xAODAnaHelpers/MetContainer.h"
 #include "xAODAnaHelpers/JetContainer.h"
 #include "xAODAnaHelpers/TruthContainer.h"
 #include "xAODAnaHelpers/MuonContainer.h"
@@ -107,6 +106,7 @@ public:
   HelperClasses::JetInfoSwitch*        m_fatJetInfoSwitch;
   HelperClasses::JetInfoSwitch*        m_truthFatJetInfoSwitch;
   HelperClasses::TauInfoSwitch*        m_tauInfoSwitch;
+  HelperClasses::METInfoSwitch*        m_metInfoSwitch;
 
 
   std::string                  m_triggerSelection;
@@ -530,7 +530,26 @@ protected:
   std::vector<float> m_tau_charge;
 
   // met
-  xAH::MetContainer*      m_met;
+  float m_metFinalClus;
+  float m_metFinalClusPx;
+  float m_metFinalClusPy;
+  float m_metFinalClusPhi;
+  float m_metFinalClusSumEt;
+
+  float m_metFinalTrk;
+  float m_metFinalTrkPx;
+  float m_metFinalTrkPy;
+  float m_metFinalTrkPhi;
+  float m_metFinalTrkSumEt;
+
+  float m_metEle;       float m_metEleSumEt;      float m_metElePhi;
+  float m_metGamma;     float m_metGammaSumEt;    float m_metGammaPhi;
+  float m_metTau;       float m_metTauSumEt;      float m_metTauPhi;
+  float m_metMuons;     float m_metMuonsSumEt;    float m_metMuonsPhi;
+  float m_metJet;       float m_metJetSumEt;      float m_metJetPhi;
+  float m_metJetTrk;    float m_metJetTrkSumEt;   float m_metJetTrkPhi;
+  float m_metSoftClus;  float m_metSoftClusSumEt; float m_metSoftClusPhi;
+  float m_metSoftTrk;   float m_metSoftTrkSumEt;  float m_metSoftTrkPhi;
 
 };
 
