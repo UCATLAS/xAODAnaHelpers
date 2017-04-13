aff7d8cd0518c2907a058eae6c5d290dd195c5d8
Giordon Stark <kratsg@users.noreply.github.com>
2017-04-13 18:35:00 -0500

    Migrate wiki fully to header documentation (#864)
    * migrate BasicEventSelection
    
    * migrate electroncalibrator
    
    * migrate electronefficiencycorrector
    
    * migrate electronselector
    
    * migrate jetcalibrator

--- xAODAnaHelpers/ElectronCalibrator.h
+++ xAODAnaHelpers/ElectronCalibrator.h
@@ -15,26 +15,52 @@
 // algorithm wrapper
 #include "xAODAnaHelpers/Algorithm.h"
 
+/**
+  @rst
+    This is the algorithm class used to calibrate electrons.
+
+    In a nutshell, this algorithm performs the following actions:
+
+      - retrieves an ``xAOD::ElectronContainer`` from either ``TEvent`` or ``TStore``
+      - makes a shallow copy container and fills it with energy-and-direction calibrated electrons using the ``EgammaCalibrationAndSmearingTool`` in `Tools Used <ToolsUsed.html>`__
+      - saves the shallow copy container to ``TStore`` from where it can be retrieved by algorithms downstream via name lookup
+
+  @endrst
+*/
 class ElectronCalibrator : public xAH::Algorithm
 {
   // put your configuration variables here as public variables.
   // that way they can be set directly from CINT and python.
 public:
-  // configuration variables
+// configuration variables
+  /// @brief The name of the input container for this algorithm to read from ``TEvent`` or ``TStore``
   std::string m_inContainerName;
+  /**
+      @brief The name of the nominal output container written by the algorithm to ``TStore``
+
+      If the algorithm applies systematic variations, for each shallow copy saved to ``TStore``, the systematic name will be appended to this.
+  */
   std::string m_outContainerName;
 
-  // sort after calibration
+  /// Sort the processed container elements by transverse momentum
   bool    m_sort;
 
 
-  // systematics
-  std::string m_inputAlgoSystNames;  // this is the name of the vector of names of the systematically varied containers produced by the
-  			             // upstream algo (e.g., the SC containers with calibration systematics)
-  std::string m_outputAlgoSystNames; // this is the name of the vector of names of the systematically varied containers produced by THIS
-  				     // algo ( these will be the m_inputAlgoSystNames of the algo downstream
-  float       m_systVal;
-  std::string m_systName;
+// systematics
+  /**
+    @brief The name of the vector containing the names of the systematically-varied containers from the upstream algorithm, which will be processed by this algorithm.
+
+    This vector is retrieved from the ``TStore``. If left blank, it means there is no upstream algorithm which applies systematics. This is the case when processing straight from the original ``xAOD`` or ``DxAOD``.
+  */
+  std::string m_inputAlgoSystNames;
+  /**
+    @brief The name of the vector containing the names of the systematically-varied containers created by by this algorithm.
+
+    @rst
+      If :cpp:member:`~xAH::Algorithm::m_systName` is empty, the vector will contain only an empty string. When running on systematics, this is the string a downstream algorithm needs to process electrons.
+    @endrst
+  */
+  std::string m_outputAlgoSystNames;
 
   std::string m_esModel;
   std::string m_decorrelationModel;
@@ -59,7 +85,7 @@ private:
 
   // tools
   CP::EgammaCalibrationAndSmearingTool *m_EgammaCalibrationAndSmearingTool; //!
-  CP::IsolationCorrectionTool          *m_IsolationCorrectionTool;          //! /* apply leakage correction to calo based isolation variables for electrons */
+  CP::IsolationCorrectionTool          *m_IsolationCorrectionTool;          //! // apply leakage correction to calo based isolation variables for electrons
 
   // variables that don't get filled at submission time should be
   // protected from being send from the submission node to the worker
--- xAODAnaHelpers/JetCalibrator.h
+++ xAODAnaHelpers/JetCalibrator.h
@@ -28,49 +28,93 @@
 // algorithm wrapper
 #include "xAODAnaHelpers/Algorithm.h"
 
+/** @rst
+  A wrapper to a few JetETMiss packages. By setting the configuration parameters detailed in the header documentation, one can:
+
+    - calibrate a given jet collection
+    - apply systematic variations for JES
+    - apply systematic variations for JER
+    - decorate the jet with the decision of the Jet Cleaning tool
+
+  When considering systematics, a new ``xAOD::JetCollection`` is created for each systematic variation. The names are then saved in a vector for downstream algorithms to use.
+
+@endrst */
 class JetCalibrator : public xAH::Algorithm
 {
-  // put your configuration variables here as public variables.
-  // that way they can be set directly from CINT and python.
 public:
-  // configuration variables
+  /// @brief The name of the input container for this algorithm to read from ``TEvent`` or ``TStore``
   std::string m_inContainerName;
+  /**
+      @brief The name of the nominal output container written by the algorithm to ``TStore``
+
+      If the algorithm applies systematic variations, for each shallow copy saved to ``TStore``, the systematic name will be appended to this.
+  */
   std::string m_outContainerName;
 
+  /// @brief set to ``AntiKt4EMTopo`` for ``AntiKt4EMTopoJets``
   std::string m_jetAlgo;
+  /// @brief name of vector holding names of jet systematics given by the JetEtmiss Tools
   std::string m_outputAlgo;
+  /// @brief config for JetCalibrationTool for Data
   std::string m_calibConfigData;
+  /// @brief config for JetCalibrationTool for Full Sim MC
   std::string m_calibConfigFullSim;
+  /// @brief config for JetCalibrationTool for AFII MC
   std::string m_calibConfigAFII;
+  /// @brief config files actually passed to JetCalibrationTool chosen from the above depending on what information stored in the input file
   std::string m_calibConfig;
+  /// @brief List of calibration steps. "Insitu" added automatically if running on data
   std::string m_calibSequence;
+  /// @brief config for JES Uncertainty Tool
   std::string m_JESUncertConfig;
+  /// @brief JetUncertaintiesTool parameter
   std::string m_JESUncertMCType;
+  /** @rst
+    If you do not want to use SampleHandler to mark samples as AFII, this flag can be used to force run the AFII configurations.
+
+    With SampleHandler, one can define sample metadata in job steering macro. You can do this with relevant samples doing something like:
+
+    .. code-block:: c++
+
+      // access a single sample
+      Sample *sample = sh.get ("mc14_13TeV.blahblahblah");
+      sample->setMetaString("SimulationFlavour", "AFII");
+
+  @endrst */
   bool m_setAFII;
   bool m_forceInsitu;
 
-  bool m_isTrigger; // whether the jet collection is trigger or not (soon: different calibrations)
+  /// @brief whether the jet collection is trigger or not (soon: different calibrations)
+  bool m_isTrigger;
 
+  // @brief Config for JER Uncert Tool. If not empty the tool will run
   std::string m_JERUncertConfig;
+  /// @brief Set systematic mode as Full (true) or Simple (false)
   bool m_JERFullSys;
+  /// @brief Apply nominal smearing
   bool m_JERApplyNominal;
 
-  /** @brief enable to apply jet cleaning decoration */
+  /// @brief enable to apply jet cleaning decoration
   bool m_doCleaning;
+  /// @brief Cut Level
   std::string m_jetCleanCutLevel;
+  /// @brief Save all cleaning decisions as decorators
   bool m_saveAllCleanDecisions;
+  /// @brief Do Ugly cleaning ( i.e. TileGap 3 )
   bool m_jetCleanUgly;
+  /// @brief Recalculate JVT using the calibrated jet pT
   bool m_redoJVT;
-  // sort after calibration
+  /// @brief Sort the processed container elements by transverse momentum
   bool    m_sort;
-  //Apply jet cleaning to parent jet
+  /// @brief Apply jet cleaning to parent jet
   bool    m_cleanParent;
   bool    m_applyFatJetPreSel;
 
-  // systematics
+// systematics
+  /// @brief set to true if systematics asked for and exist
   bool m_runSysts;
 
-  // jet tile correction
+  /// @brief jet tile correction
   bool m_doJetTileCorr;
 
 private:
@@ -99,13 +143,7 @@ private:
   asg::AnaToolHandle<CP::IJetTileCorrectionTool> m_JetTileCorrectionTool_handle; //!
 
 
-  // variables that don't get filled at submission time should be
-  // protected from being send from the submission node to the worker
-  // node (done by the //!)
 public:
-  // Tree *myTree; //!
-  // TH1 *myHist; //!
-
 
   // this is a standard constructor
   JetCalibrator (std::string className = "JetCalibrator");
