dc19f86c98e5edd0f0fa0eb7bea9c54622ffd689
Giordon Stark <kratsg@gmail.com>
2017-07-24 23:45:28 -0500

    add traceback to help identify where import is

--- scripts/xAH_config.py
+++ scripts/xAH_config.py
@@ -3,10 +3,13 @@
 from __future__ import absolute_import
 from __future__ import print_function
 import logging
+logger = logging.getLogger("xAH.config")
 
 from xAODAnaHelpers import Config as xAH_config
 
-logger = logging.getLogger("xAH.config")
-logger.setLevel(10) # we use info
+import inspect
 
-logger.warning("xAH_config is being deprecated. Use 'from xAODAnaHelpers import Config as xAH_config' to maintain backwards compatibility... or 'from xAODAnaHelpers import Config' and rename 'xAH_config' to 'Config'.")
+logger.warning("xAH_config is being deprecated. Use 'from xAODAnaHelpers import Config as xAH_config' to maintain backwards compatibility... or 'from xAODAnaHelpers import Config' and rename 'xAH_config' to 'Config'. Possible call stack:")
+for frame, path, lineno, source, lines, _ in inspect.stack()[1:]:
+  if 'importhook' in source or path.endswith('ROOT.py'): continue
+  logger.warning('\t{0:s}({1:d}): {2:s}'.format(path, lineno, lines[0].strip()))
