38c026728f04dc51a4c2e107039c245e9cbbbf9b
Giordon Stark <kratsg@gmail.com>
2017-09-12 16:20:51 -0500

    don't recursive search

--- ci/run.sh
+++ ci/run.sh
@@ -2,7 +2,7 @@
 source /home/atlas/release_setup.sh
 set -ev
 # make a symbolic link for a top-level CMakeLists.txt file
-ln -s /workarea/src/xAODAnaHelpers/ci/CMakeLists.txt /workarea/src/CMakeLists.txt
+ln -s /workarea/src/xAODAnaHelpers/ci/top_CMakeLists.txt /workarea/src/CMakeLists.txt
 # make the build directory and start doing stuff
 mkdir -p /workarea/build
 cd /workarea/build
