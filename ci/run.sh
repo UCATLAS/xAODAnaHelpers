f2a4a9dab648f9a849ef0397c158983915c9d7f0
Giordon Stark <kratsg@gmail.com>
2017-09-12 15:58:26 -0500

    run as root, make the log less verbose

--- ci/run.sh
+++ ci/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-set -ev
 source /home/atlas/release_setup.sh
-mkdir -p /workarea/build
-cd /workarea/build
+set -ev
 # make a symbolic link for a top-level CMakeLists.txt file
 ln -s /workarea/src/xAODAnaHelpers/ci/CMakeLists.txt /workarea/src/CMakeLists.txt
+# make the build directory and start doing stuff
+mkdir -p /workarea/build
+cd /workarea/build
 cmake ../src
 make
 cd ../
