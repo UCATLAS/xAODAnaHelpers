436d59fa925a7b6f525027808741cbdc11f3a48b
Giordon Stark <kratsg@users.noreply.github.com>
2017-09-13 15:59:57 -0500

    Travis CI CMake testing (for Rel 21!) (#1028)
    * can we use travis for CMake building/testing? yes!
    * add explicit env variables to matrix includes
    * run as root
    * add a hack for /tmp because of aufs
    * add allowed failures 
    * make docs build correctly due to change location for versioning

---
+++ ci/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source /home/atlas/release_setup.sh
+set -ev
+# make a symbolic link for a top-level CMakeLists.txt file
+ln -s /workarea/src/xAODAnaHelpers/ci/top_CMakeLists.txt /workarea/src/CMakeLists.txt
+# make the build directory and start doing stuff
+mkdir -p /workarea/build
+cd /workarea/build
+cmake ../src
+time make
+cd ../
+printenv
+set +ev
+source build/${AnalysisBase_PLATFORM}/setup.sh
+set -ev
+python -c "import ROOT; print ROOT.xAH"
