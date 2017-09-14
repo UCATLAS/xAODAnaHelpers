2b7f1c9a49fd0510277a71fb05cea58d45a5b271
Giordon Stark <kratsg@gmail.com>
2017-09-14 10:30:48 -0500

    try overriding TMPDIR

--- ci/compile.sh
+++ ci/compile.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 source /home/atlas/release_setup.sh
 set -ev
+# set tmpdir in a writeable location
+mkdir -p /workarea/tmp/
+export TMPDIR=/workarea/tmp/
 # make a symbolic link for a top-level CMakeLists.txt file
 ln -s /workarea/src/xAODAnaHelpers/ci/top_CMakeLists.txt /workarea/src/CMakeLists.txt
 # make the build directory and start doing stuff
