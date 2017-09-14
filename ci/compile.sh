42e78688afdf4c016b29f697ce39530ec6eb5672
Giordon Stark <kratsg@gmail.com>
2017-09-14 16:37:47 -0500

    move ENV over

--- ci/compile.sh
+++ ci/compile.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 source /home/atlas/release_setup.sh
 set -ev
-# set tmpdir in a writeable location
-mkdir -p /workarea/tmp/
-export TMPDIR=/workarea/tmp/
 # make a symbolic link for a top-level CMakeLists.txt file
 ln -s /workarea/src/xAODAnaHelpers/ci/top_CMakeLists.txt /workarea/src/CMakeLists.txt
 # make the build directory and start doing stuff
@@ -14,3 +11,4 @@ time make
 #time cpack
 cd ../
 printenv
+set +ev
