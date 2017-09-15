86e06fb4bbc0e769dd86598a98894cf2fcd2304e
Giordon Stark <kratsg@gmail.com>
2017-09-15 11:06:15 -0500

    change to <release>-<hash>-<date> and add <release>-latest tags

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker login -u $DOCKERLOGIN -p $DOCKERPW
-docker tag ucatlas/xah ucatlas/xah:$(date +%Y%M%d)-${ABV_CM}-${TRAVIS_COMMIT::6}
-docker push ucatlas/xah:$(date +%Y%M%d)-${ABV_CM}-${TRAVIS_COMMIT::6}
+docker tag  ucatlas/xah:${ABV_CM}-latest ucatlas/xah:${ABV_CM}-${TRAVIS_COMMIT::6}-$(date +%Y%M%d)
+docker push ucatlas/xah:${ABV_CM}-${TRAVIS_COMMIT::6}-$(date +%Y%M%d)
+docker push ucatlas/xah:${ABV_CM}-latest
