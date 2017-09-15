03408a737e9dcae1b7886a7b3b8529ac70deeeb6
Giordon Stark <kratsg@gmail.com>
2017-09-15 14:02:30 -0500

    [skip ci] change terminal colors

--- ci/xAODAnaHelpers_setup.sh
+++ ci/xAODAnaHelpers_setup.sh
@@ -10,4 +10,4 @@ source /usr/xAODAnaHelpers/*/InstallArea/*/setup.sh
 echo "Configured xAODAnaHelpers from: ${xAODAnaHelpers_DIR}"
 
 # Set up the prompt:
-export PS1="\[\e[1;31;40m\]xAODAnaHelpers-${xAODAnaHelpers_VERSION::6}\[\e[0m\]:\[\e[36;40m\]\w\[\e[1;33;40m\]$ \[\e[0m\]"
+export PS1="\[\033[0;35m\][\u]\[\e[0;31m\][xAH-${xAODAnaHelpers_VERSION::6}]\[\e[0;35m\]:\[\e[0;36m\]\w\[\e[1;33m\]$ \[\e[0m\]"
