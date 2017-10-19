e22d02b410cd28dfc33216cd42c67c43e16b29f8
Giordon Stark <kratsg@users.noreply.github.com>
2017-10-19 10:51:26 -0500

    [skip ci] use the correct date format (#1060)

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker login -u $DOCKERLOGIN -p $DOCKERPW
-docker tag  ucatlas/xah:${ABV_CM}-latest ucatlas/xah:${ABV_CM}-${TRAVIS_COMMIT::6}-$(date +%Y%M%d)
-docker push ucatlas/xah:${ABV_CM}-${TRAVIS_COMMIT::6}-$(date +%Y%M%d)
+docker tag  ucatlas/xah:${ABV_CM}-latest ucatlas/xah:${ABV_CM}-${TRAVIS_COMMIT::6}-$(date +%Y%m%d)
+docker push ucatlas/xah:${ABV_CM}-${TRAVIS_COMMIT::6}-$(date +%Y%m%d)
 docker push ucatlas/xah:${ABV_CM}-latest
