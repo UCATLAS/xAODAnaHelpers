921a271f1bd910b77bea6e79aadf8c99ca99932e
Giordon Stark <kratsg@users.noreply.github.com>
2018-01-05 11:18:26 -0500

    deploy to multiple repos instead (#1126)

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker login -u $DOCKERLOGIN -p $DOCKERPW
-docker tag  ucatlas/xah:${DOCKER_TAG}-latest ucatlas/xah:${DOCKER_TAG}-${TRAVIS_COMMIT::6}-$(date +%Y%m%d)
-docker push ucatlas/xah:${DOCKER_TAG}-${TRAVIS_COMMIT::6}-$(date +%Y%m%d)
-docker push ucatlas/xah:${DOCKER_TAG}-latest
+docker tag  ${DOCKER_ORG}/${DOCKER_REPO}:${DOCKER_TAG}-latest ${DOCKER_ORG}/${DOCKER_REPO}:${DOCKER_TAG}-${TRAVIS_COMMIT::6}-$(date +%Y%m%d)
+docker push ${DOCKER_ORG}/${DOCKER_REPO}:${DOCKER_TAG}-${TRAVIS_COMMIT::6}-$(date +%Y%m%d)
+docker push ${DOCKER_ORG}/${DOCKER_REPO}:${DOCKER_TAG}-latest
