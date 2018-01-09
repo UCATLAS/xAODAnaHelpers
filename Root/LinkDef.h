e2e2bc183318675f8a73758cc28ae12d2275e18f
Federico <federicoscutti@gmail.com>
2018-01-09 11:28:12 +0000

    started tau development

--- Root/LinkDef.h
+++ Root/LinkDef.h
@@ -17,6 +17,7 @@
 #include <xAODAnaHelpers/PhotonCalibrator.h>
 #include <xAODAnaHelpers/JetCalibrator.h>
 #include <xAODAnaHelpers/MuonCalibrator.h>
+#include <xAODAnaHelpers/TauCalibrator.h>
 /*#include <xAODAnaHelpers/GroomedFatJets.h>*/
 #include <xAODAnaHelpers/HLTJetRoIBuilder.h>
 #include <xAODAnaHelpers/HLTJetGetter.h>
@@ -75,6 +76,7 @@
 #pragma link C++ class JetCalibrator+;
 #pragma link C++ class HLTJetRoIBuilder+;
 #pragma link C++ class MuonCalibrator+;
+#pragma link C++ class TauCalibrator+;
 #pragma link C++ class HLTJetGetter+;
 /*#pragma link C++ class GroomedFatJets+;*/
 
