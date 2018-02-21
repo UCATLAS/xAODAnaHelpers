35ecda45d256e9522b511e7dc8bba876397c2277
Charles William Kalderon <william.kalderon@cern.ch>
2018-02-21 17:26:15 +0100

    Option to ignore lack of Primary Vertices DataScouting does not have tracks or vertices, and so PV checks were causing crashes. Added an option to skip PV checks, and then downgrade a warning to debug in HelperFunctions::getPrimaryVertexLocation to avoid spam. There are perhaps better ways to deal with the latter issue that will retain the warning for most users.

--- xAODAnaHelpers/HelperClasses.h
+++ xAODAnaHelpers/HelperClasses.h
@@ -401,6 +401,7 @@ namespace HelperClasses {
         m_rapidity       rapidity       exact
         m_clean          clean          exact
         m_cleanLight     cleanLight     exact
+        m_cleanNoSumm    cleanNoSumm    exact
         m_energy         energy         exact
         m_energyLight    energyLight    exact
         m_scales         scales         exact
@@ -474,6 +475,7 @@ namespace HelperClasses {
     bool m_rapidity;
     bool m_clean;
     bool m_cleanLight;
+    bool m_cleanNoSumm;
     bool m_energy;
     bool m_energyLight;
     bool m_scales;
