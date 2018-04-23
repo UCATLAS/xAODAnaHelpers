d529a43a93f1e846627a616352eaf93f0126e6b9
Leigh Catherine Schaefer <leigh.schaefer@cern.ch>
2018-04-23 18:04:15 -0400

    move btag decorator outside of systematics loop

--- Root/BJetEfficiencyCorrector.cxx
+++ Root/BJetEfficiencyCorrector.cxx
@@ -413,6 +413,7 @@ EL::StatusCode BJetEfficiencyCorrector :: executeEfficiencyCorrection(const xAOD
     dec_sfBTag( *jet_itr ) = sfVec;
   }
 
+
   //
   // Store list of available systematics
   //
