6829f7508be57812abfa05a13f3359e009847bfd
Leigh Catherine Schaefer <leigh.schaefer@cern.ch>
2018-04-27 16:00:59 -0400

    update BJetEfficiencyCorrector with getListOfSystematics

--- Root/BJetEfficiencyCorrector.cxx
+++ Root/BJetEfficiencyCorrector.cxx
@@ -413,7 +413,6 @@ EL::StatusCode BJetEfficiencyCorrector :: executeEfficiencyCorrection(const xAOD
     idx++;
     dec_sfBTag( *jet_itr ) = sfVec;
   }
-
   //
   // Store list of available systematics
   //
