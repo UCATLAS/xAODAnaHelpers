e48d85a7e8d1280f84cef530648f2e7d2dc44f0e
Jonathan Bossio <bossjonad@gmail.com>
2019-10-15 19:03:35 +0200

    Remove muons reconstructed as PF jets in OR (#1368)
    * Remove muons reconstructed as PF jets in OR
    
    Following JetEtmiss recs:
    https://twiki.cern.ch/twiki/bin/view/AtlasProtected/HowToCleanJetsR21#Muons_Reconstructed_as_Jets_in_P
    
    * Simplification

--- Root/OverlapRemover.cxx
+++ Root/OverlapRemover.cxx
@@ -162,6 +162,7 @@ EL::StatusCode OverlapRemover :: initialize ()
   orFlags.doTaus      = m_useTaus;
   orFlags.doPhotons   = m_usePhotons;
   orFlags.doFatJets   = false;
+  orFlags.doMuPFJetOR = m_doMuPFJetOR;
 
   ANA_CHECK( ORUtils::recommendedTools(orFlags, m_ORToolbox));
   ANA_CHECK( m_ORToolbox.initialize());
