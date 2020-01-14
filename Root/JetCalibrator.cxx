53db1b1e827c89043bad8226cf001bbf6485aa11
mtoscani <mtoscani@cern.ch>
2020-01-14 17:22:03 +0100

    Calling PathResolverFindCalibDirectory only once

--- Root/JetCalibrator.cxx
+++ Root/JetCalibrator.cxx
@@ -299,8 +299,9 @@ EL::StatusCode JetCalibrator :: initialize ()
       ANA_CHECK( m_JetUncertaintiesTool_handle.setProperty("AnalysisFile", m_overrideAnalysisFile));
     }
     if( !m_overrideUncertPath.empty() ){
-      ANA_MSG_WARNING("Overriding jet uncertainties path to " << PathResolverFindCalibDirectory(m_overrideUncertPath));
-      ANA_CHECK( m_JetUncertaintiesTool_handle.setProperty("Path", PathResolverFindCalibDirectory(m_overrideUncertPath)));
+      std::string uncPath = PathResolverFindCalibDirectory(m_overrideUncertPath);
+      ANA_MSG_WARNING("Overriding jet uncertainties path to " << uncPath);
+      ANA_CHECK( m_JetUncertaintiesTool_handle.setProperty("Path", uncPath));
     }
     ANA_CHECK( m_JetUncertaintiesTool_handle.setProperty("OutputLevel", msg().level()));
     ANA_CHECK( m_JetUncertaintiesTool_handle.retrieve());
