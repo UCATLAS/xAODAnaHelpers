012240a5d69df8ae1caff58c1a70c2f4d12b761c
Giordon Stark <kratsg@users.noreply.github.com>
2020-03-25 01:18:25 -0700

    harmonize m_pass_max (#1429)

--- Root/JetSelector.cxx
+++ Root/JetSelector.cxx
@@ -931,7 +931,7 @@ bool JetSelector :: executeSelection ( const xAOD::JetContainer* inJets,
   if ( m_pass_min > 0 && nPass < m_pass_min ) {
     return false;
   }
-  if ( m_pass_max > 0 && nPass > m_pass_max ) {
+  if ( m_pass_max >= 0 && nPass > m_pass_max ) {
     return false;
   }
 
