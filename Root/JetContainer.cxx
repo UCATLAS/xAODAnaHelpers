4b140cf1f0b435cdcabf730c6bb5af8612915f51
Matthew Feickert <matthew.feickert@cern.ch>
2020-08-19 00:24:10 -0500

    Add calculation of ChargeFrac
    no need to divide by m_units as if done both times will cancel out

--- Root/JetContainer.cxx
+++ Root/JetContainer.cxx
@@ -39,6 +39,7 @@ JetContainer::JetContainer(const std::string& name, const std::string& detailStr
       m_N90Constituents           =new std::vector<float>();
       m_LArBadHVEnergyFrac        =new std::vector<float>();
       m_LArBadHVNCell             =new std::vector<int>();
+      m_ChargeFrac                =new std::vector<float>();
       m_OotFracClusters5          =new std::vector<float>();
       m_OotFracClusters10         =new std::vector<float>();
       m_LeadingClusterPt          =new std::vector<float>();
@@ -466,6 +467,7 @@ JetContainer::~JetContainer()
       delete m_N90Constituents;
       delete m_LArBadHVEnergyFrac;
       delete m_LArBadHVNCell;
+      delete m_ChargeFrac;
       delete m_OotFracClusters5;
       delete m_OotFracClusters10;
       delete m_LeadingClusterPt;
@@ -883,6 +885,7 @@ void JetContainer::setTree(TTree *tree)
         connectBranch<float>(tree, "BchCorrCell",                &m_BchCorrCell);
         connectBranch<float>(tree, "N90Constituents",            &m_N90Constituents);
         connectBranch<float>(tree, "LArBadHVEnergyFrac",         &m_LArBadHVEnergyFrac);
+        connectBranch<float>(tree, "ChargeFrac",                 &m_ChargeFrac);
         connectBranch<float>(tree, "OotFracClusters5",           &m_OotFracClusters5);
         connectBranch<float>(tree, "OotFracClusters10",          &m_OotFracClusters10);
         connectBranch<float>(tree, "LeadingClusterPt",           &m_LeadingClusterPt);
@@ -1135,7 +1138,7 @@ void JetContainer::updateParticle(uint idx, Jet& jet)
         jet.AverageLArQF              =m_AverageLArQF              ->at(idx);
         jet.BchCorrCell               =m_BchCorrCell               ->at(idx);
         jet.N90Constituents           =m_N90Constituents           ->at(idx);
-        jet.LArBadHVEFrac             =m_LArBadHVEnergyFrac       ->at(idx);
+        jet.LArBadHVEFrac             =m_LArBadHVEnergyFrac        ->at(idx);
         jet.LArBadHVNCell             =m_LArBadHVNCell             ->at(idx);
         jet.OotFracClusters5          =m_OotFracClusters5          ->at(idx);
         jet.OotFracClusters10         =m_OotFracClusters10         ->at(idx);
@@ -1491,6 +1494,7 @@ void JetContainer::setBranches(TTree *tree)
       setBranch<float>(tree,"N90Constituents",               m_N90Constituents           );
       setBranch<float>(tree,"LArBadHVEnergyFrac",            m_LArBadHVEnergyFrac   );
       setBranch<int>  (tree,"LArBadHVNCell",                 m_LArBadHVNCell  	  );
+      setBranch<float>(tree,"ChargeFrac",                    m_ChargeFrac);
       setBranch<float>(tree,"OotFracClusters5",              m_OotFracClusters5  	    );
       setBranch<float>(tree,"OotFracClusters10",             m_OotFracClusters10  	  );
       setBranch<float>(tree,"LeadingClusterPt",              m_LeadingClusterPt  	            );
@@ -1901,6 +1905,7 @@ void JetContainer::clear()
       m_N90Constituents           ->clear();
       m_LArBadHVEnergyFrac        ->clear();
       m_LArBadHVNCell             ->clear();
+      m_ChargeFrac                ->clear();
       m_OotFracClusters5          ->clear();
       m_OotFracClusters10         ->clear();
       m_LeadingClusterPt          ->clear();
@@ -2544,7 +2549,7 @@ void JetContainer::FillJet( const xAOD::IParticle* particle, const xAOD::Vertex*
     }
   }
 
-  if ( m_infoSwitch.m_trackAll || m_infoSwitch.m_trackPV || m_infoSwitch.m_jvt ) {
+  if ( m_infoSwitch.m_trackAll || m_infoSwitch.m_trackPV || m_infoSwitch.m_jvt || m_infoSwitch.m_clean ) {
 
     // several moments calculated from all verticies
     // one accessor for each and just use appropiately in the following
@@ -2648,6 +2653,12 @@ void JetContainer::FillJet( const xAOD::IParticle* particle, const xAOD::Vertex*
       //      } else { m_ghostTrackAssFrac->push_back( -999 ) ; }
 
     } // trackPV || JVT
+
+    if ( m_infoSwitch.m_clean && pvLocation >= 0 ) {
+      if ( sumPt500.isAvailable( *jet ) ) {
+        m_ChargeFrac->push_back( sumPt500( *jet )[pvLocation] / jet->pt() ); // units cancel out
+      } else { m_ChargeFrac->push_back( -999 ); }
+    } // clean
 
   } // trackAll || trackPV || JVT
 
