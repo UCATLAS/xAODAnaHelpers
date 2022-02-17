97de85d21f9f1333f212819254e5a9c3fcf134e5
Darij Markian Starko <darij.markian.starko@cern.ch>
2022-02-17 16:49:43 +0100

    fixed jet cleaning

--- Root/HelperClasses.cxx
+++ Root/HelperClasses.cxx
@@ -261,6 +261,7 @@ namespace HelperClasses{
     m_rapidity      = has_exact("rapidity");
     m_clean         = has_exact("clean");
     m_cleanLight    = has_exact("cleanLight");
+    m_cleanLLP      = has_exact("cleanLLP");
     m_cleanTrig     = has_exact("cleanTrig");
     m_energy        = has_exact("energy");
     m_energyLight   = has_exact("energyLight");
