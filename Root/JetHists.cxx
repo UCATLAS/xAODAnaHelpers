dd31ddf651081646dfd6fbbda590f74e588614f5
Hamza <hamza.hanif@cern.ch>
2022-04-13 11:26:22 +0200

    changed the x-max of pt-rel histogram

--- Root/JetHists.cxx
+++ Root/JetHists.cxx
@@ -65,7 +65,7 @@ StatusCode JetHists::initialize() {
     m_chf         = book(m_name, "chg",          m_titlePrefix+"Charge Hadron Fraction" ,    100, 0, 1);
     m_dchf        = book(m_name, "dchf",          m_titlePrefix+"Displaced Charge Hadron Fraction" ,    100, 0, 1);
     m_alpha_max   = book(m_name, "alpha_max",      m_titlePrefix+"#alpha_{max}" , 100, 0, 1);
-    m_ptrel       = book(m_name,  "ptrel",         m_titlePrefix+"Relative momentum" ,    90, 0, 2500);
+    m_ptrel       = book(m_name,  "ptrel",         m_titlePrefix+"Relative momentum" ,    90, 0, 100);
 
   }
 
