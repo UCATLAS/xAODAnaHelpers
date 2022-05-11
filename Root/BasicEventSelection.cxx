59acc6448283c0f21ce96fa34a068f8481b8b368
Jonathan Bossio <bossjonad@gmail.com>
2022-05-11 17:52:45 +0200

    Avoid crash in DAOD_PHYS data, set new fJVT property and update setting of muon calibration tool (#1537)
    * Set new fJVT property
    
    * Avoid crash in data new prop fJVT and update muons
    
    streamTags() crashes in DAOD_PHYS data. This is only used for debugging.
    It is now turned off by default
    
    Set new fJVT property
    
    Update setting of muon calibration tool

--- Root/BasicEventSelection.cxx
+++ Root/BasicEventSelection.cxx
@@ -853,10 +853,12 @@ EL::StatusCode BasicEventSelection :: execute ()
   if ( !isMC() ) {
 
     // Get the streams that the event was put in
-    const std::vector<  xAOD::EventInfo::StreamTag > streams = eventInfo->streamTags();
+    if (m_checkStreams){
+      const std::vector<  xAOD::EventInfo::StreamTag > streams = eventInfo->streamTags();
 
-    for ( auto& stream : streams ) {
-      ANA_MSG_DEBUG( "event has fired stream: " << stream.name() );
+      for ( auto& stream : streams ) {
+        ANA_MSG_DEBUG( "event has fired stream: " << stream.name() );
+      }
     }
 
     // GRL
