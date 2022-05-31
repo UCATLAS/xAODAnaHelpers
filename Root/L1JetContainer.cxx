0b8c93fa92c9216214de348b2ee16c24fa22c865
Ricardo Barrue <ricardo.barrue@tecnico.ulisboa.pt>
2022-05-31 09:04:31 +0100

    Adding first implementation of access to Phase1 L1Calo jet containers

--- Root/L1JetContainer.cxx
+++ Root/L1JetContainer.cxx
@@ -6,7 +6,7 @@ using namespace xAH;
 L1JetContainer::L1JetContainer(const std::string& name, float units, bool mc)
   : ParticleContainer(name,"",units,mc)
 {
-  m_l1Jet_et8x8             =new std::vector<float>();
+  m_l1Jet_et                =new std::vector<float>();
   m_l1Jet_eta               =new std::vector<float>();
   m_l1Jet_phi               =new std::vector<float>();
 }
@@ -14,7 +14,7 @@ L1JetContainer::L1JetContainer(const std::string& name, float units, bool mc)
 L1JetContainer::~L1JetContainer()
 {
   if(m_debug) std::cout << " Deleting L1JetContainer "  << std::endl;
-  delete m_l1Jet_et8x8;
+  delete m_l1Jet_et;
   delete m_l1Jet_eta;
   delete m_l1Jet_phi;
 }
@@ -22,7 +22,7 @@ L1JetContainer::~L1JetContainer()
 void L1JetContainer::setTree(TTree *tree)
 {
   ParticleContainer::setTree(tree);
-  connectBranch<float>(tree,"et8x8",&m_l1Jet_et8x8);
+  connectBranch<float>(tree,"et",&m_l1Jet_et);
   connectBranch<float>(tree,"eta",  &m_l1Jet_eta);
   connectBranch<float>(tree,"phi",  &m_l1Jet_phi);
 }
@@ -35,7 +35,7 @@ void L1JetContainer::updateParticle(uint idx, Jet& jet)
 void L1JetContainer::setBranches(TTree *tree)
 {
   ParticleContainer::setBranches(tree);
-  setBranch<float>(tree,"et8x8",m_l1Jet_et8x8);
+  setBranch<float>(tree,"et",m_l1Jet_et); // et8x8 for the case of Legacy L1 jet RoIs
   setBranch<float>(tree,"eta",  m_l1Jet_eta);
   setBranch<float>(tree,"phi",  m_l1Jet_phi);
   return;
@@ -44,16 +44,16 @@ void L1JetContainer::setBranches(TTree *tree)
 void L1JetContainer::clear()
 {
   ParticleContainer::clear();
-  m_l1Jet_et8x8->clear();
+  m_l1Jet_et->clear();
   m_l1Jet_eta  ->clear();
   m_l1Jet_phi  ->clear();
   return;
 }
 
-void L1JetContainer::FillL1Jets( const xAOD::JetRoIContainer* jets, bool sort){
+void L1JetContainer::FillLegacyL1Jets( const xAOD::JetRoIContainer* jets, bool sort){
   if(!sort) {
     for( auto jet_itr : *jets ) {
-      m_l1Jet_et8x8->push_back ( jet_itr->et8x8() / m_units );
+      m_l1Jet_et->push_back ( jet_itr->et8x8() / m_units );
       m_l1Jet_eta->push_back( jet_itr->eta() );
       m_l1Jet_phi->push_back( jet_itr->phi() );
     }
@@ -70,7 +70,7 @@ void L1JetContainer::FillL1Jets( const xAOD::JetRoIContainer* jets, bool sort){
     
     std::sort(vec.begin(), vec.end(), [&](const std::vector<float> a, const std::vector<float> b) { return a.at(0) < b.at(0);});
     for (int i = int(vec.size())-1; i >= 0; i--) {
-      m_l1Jet_et8x8->push_back((vec.at(i)).at(0) / m_units);
+      m_l1Jet_et->push_back((vec.at(i)).at(0) / m_units);
       m_l1Jet_eta->push_back((vec.at(i)).at(1));
       m_l1Jet_phi->push_back((vec.at(i)).at(2));
     }
